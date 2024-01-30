fbb39437da3cc5547822f8c8fc2085bf67f69d6b
Alina Shen <msa316@sfu.ca>
2024-01-30 13:44:27 -0800

    init push

---
+++ src/main/java/cmpt276/asn1/InteractiveQuizApplication.java
@@ -0,0 +1,13 @@
+package cmpt276.asn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveQuizApplication.class, args);
+	}
+
+}
